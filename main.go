ad9c07c7423d89c3c94a31ca3fab9708a6d51740
Paul DiOrio <none>
2021-09-01 04:02:48 -0400

    Change conf example name to gmail

--- main.go
+++ main.go
@@ -9,14 +9,14 @@ import (
 
 var (
 	participantPtr *string
-	emailPtr       *string
+	gmailPtr       *string
 	seedVal        *int64
 	dryrun         *bool
 )
 
 func init() {
 	participantPtr = flag.String("participants", "conf/participants.yaml.example", "path to a yaml file with participant metadata")
-	emailPtr = flag.String("config", "conf/email.yaml.example", "path to a yaml file with email configuration")
+	gmailPtr = flag.String("config", "conf/gmail.yaml.example", "path to a yaml file with email configuration")
 	seedVal = flag.Int64("seed", 0, "initial seed value; 0 indicates initialize randomness with the current execution time")
 	dryrun = flag.Bool("dryrun", false, "execute in dryrun mode (examine secret results & do not send messages)")
 }
@@ -25,5 +25,5 @@ func main() {
 	flag.Parse()
 
 	matches := selection.RunSelection(*participantPtr, *seedVal, *dryrun)
-	notify.NotifyPariticpants(matches, *emailPtr, *dryrun)
+	notify.NotifyPariticpants(matches, *gmailPtr, *dryrun)
 }
